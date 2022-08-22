d567e07f4c382b1ed2cc7d1cf6c9e3a0d3d5821b
Noah Greifer <noah.greifer@gmail.com>
2022-08-22 13:41:15 -0400

    Updates

--- R/summary.estimateEffects.R
+++ R/summary.estimateEffects.R
@@ -1,29 +1,44 @@
 summary.frontierEstimates <- function(object, N, Ndrop, ...) {
   if (!inherits(object, "frontierEstimates")) {
-    customStop("'object' must be a matchFrontier object, the output of a call to makeFrontier().")
+    customStop("'object' must be a matchFrontier object, the output of a call to makeFrontier().",
+               "summary()")
   }
   if (missing(N) && missing(Ndrop)) {
-    customStop("one of 'N' or 'Ndrop' must be specified.")
+    N <- c(object$n - object$Xs[1],
+           round(median(object$n - object$Xs)),
+           object$n - object$Xs[length(object$Xs)])
   }
   else if (!missing(N) && !missing(Ndrop)) {
     customStop("only one of 'N' or 'Ndrop' may be specified.", "summary()")
   }
   else if (!missing(Ndrop)) {
     if (!is.numeric(Ndrop) ||
-        any(Ndrop < 0) || any(Ndrop > object$n - 1)) {
-      customStop(paste0("'Ndrop' must be between 0 and ", object$n - 1, "."), "summary()")
+        any(Ndrop < min(object$Xs)) || any(Ndrop >  max(object$Xs))) {
+      customStop(sprintf("'Ndrop' must be between %s and %s.",
+                         min(object$Xs), max(object$Xs)), "summary()")
     }
     N <- object$n - Ndrop
   }
   else if (!missing(N)) {
     if (!is.numeric(N) ||
-        any(N < 1) || any(N > object$n)) {
-      customStop(paste0("'N' must be between 1 and ", object$n, "."), "summary()")
+        any(N < min(object$n - object$Xs)) || any(N > max(object$n - object$Xs))) {
+      customStop(sprintf("'N' must be between %s and %s.",
+                         min(object$n - object$Xs), max(object$n - object$Xs)),
+                 "summary()")
     }
   }
 
   ord <- order(N, decreasing = TRUE)
   N <- N[ord]
+
+  Ns <- object$n - object$Xs
+  for (i in seq_along(N)) {
+    if (N[i] != object$n) {
+      ind <- which.min(abs(Ns - N[i]))
+      N[i] <- Ns[ind]
+    }
+  }
+  N <- unique(N)
 
   selectedEst <- numeric(length(N))
   selectedCIlower <- numeric(length(N))
@@ -31,19 +46,27 @@ summary.frontierEstimates <- function(object, N, Ndrop, ...) {
   selectedMDlower <- numeric(length(N))
   selectedMDupper <- numeric(length(N))
 
-  Ns <- object$n - object$Xs
-
   for (i in seq_along(N)) {
-    ind <- which.min(Ns[Ns >= N[i]])
-    N[i] <- Ns[ind]
+    if (N[i] == object$n) {
+      selectedEst[i] <- object$un$coef
+      selectedCIlower[i] <- object$un$CI[1]
+      selectedCIupper[i] <- object$un$CI[2]
+      if (length(object$un$mod.dependence) != 0) {
+        selectedMDlower[i] <- object$un$mod.dependence[1]
+        selectedMDupper[i] <- object$un$mod.dependence[2]
+      }
+    }
+    else {
+      ind <- which(Ns == N[i])
 
-    selectedEst[i] <- object$coefs[ind]
-    selectedCIlower[i] <- object$CIs[[ind]][1]
-    selectedCIupper[i] <- object$CIs[[ind]][2]
+      selectedEst[i] <- object$coefs[ind]
+      selectedCIlower[i] <- object$CIs[[ind]][1]
+      selectedCIupper[i] <- object$CIs[[ind]][2]
 
-    if (length(object$mod.dependence) != 0) {
-      selectedMDlower[i] <- object$mod.dependence[[ind]][1]
-      selectedMDupper[i] <- object$mod.dependence[[ind]][2]
+      if (length(object$mod.dependence) != 0) {
+        selectedMDlower[i] <- object$mod.dependence[[ind]][1]
+        selectedMDupper[i] <- object$mod.dependence[[ind]][2]
+      }
     }
   }
 
