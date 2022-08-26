b437618936b89e4d7e59f28022913efada2e97a2
Noah Greifer <noah.greifer@gmail.com>
2022-08-26 13:14:07 -0400

    Adjustments to account for matrix matched.to; changed dataset to data

--- R/makeMatchedData.R
+++ R/makeMatchedData.R
@@ -1,56 +1,69 @@
-makeMatchedData <- function(dataset, matched.to = NULL, drop.inds = NULL, weights = ".weights",
+makeMatchedData <- function(data, matched.to = NULL, drop.inds = NULL, weights = ".weights",
                             dup = FALSE, with_replacement = TRUE, subclass = ".subclass", id = ".id") {
   if (is.null(matched.to)) {
-    if (length(drop.inds) > 0) dataset <- dataset[-drop.inds,,drop=FALSE]
+    if (length(drop.inds) > 0) data <- data[-drop.inds,,drop=FALSE]
 
-    dataset[[weights]] <- 1
+    data[[weights]] <- 1
 
-    attr(dataset, "weights") <- weights
+    attr(data, "weights") <- weights
   }
   else if (dup) {
     if (length(drop.inds) != 0) {
-      is.na(matched.to[drop.inds]) <- TRUE
+      is.na(matched.to[drop.inds,]) <- TRUE
     }
 
-    s <- factor(c(which(!is.na(matched.to)), which(!is.na(matched.to))))
+    s <- factor(c(which(!is.na(matched.to[,1])),
+                  unlist(lapply(seq_len(ncol(matched.to)),
+                                function(i) which(!is.na(matched.to[,i]))))))
     levels(s) <- seq_len(nlevels(s))
-    i <- c(which(!is.na(matched.to)), matched.to[!is.na(matched.to)])
+    i <- c(which(!is.na(matched.to[,1])),
+           unlist(lapply(seq_len(ncol(matched.to)),
+                  function(i) matched.to[!is.na(matched.to[,i]), i])))
+    w <- c(rep(1, sum(!is.na(matched.to[,1]))),
+           unlist(lapply(seq_len(ncol(matched.to)),
+                         function(i) rep(1/ncol(matched.to), sum(!is.na(matched.to[,i]))))))
 
-    rn <- rownames(dataset)
-    dataset <- dataset[i,]
+    rn <- rownames(data)
+    data <- data[i,]
 
-    dataset <- setNames(cbind(rn[i], s, 1, dataset),
-                        c(id, subclass, weights, names(dataset)))
+    data <- setNames(cbind(rn[i], s, w, data),
+                        c(id, subclass, weights, names(data)))
 
-    attr(dataset, "weights") <- weights
-    attr(dataset, "subclass") <- subclass
-    attr(dataset, "id") <- id
+    attr(data, "weights") <- weights
+    attr(data, "subclass") <- subclass
+    attr(data, "id") <- id
   }
   else {
 
     if (length(drop.inds) > 0) {
-      is.na(matched.to)[drop.inds] <- TRUE
+      is.na(matched.to)[drop.inds,] <- TRUE
     }
 
-    w <- as.numeric(!is.na(matched.to)) + tabulate(matched.to, nbins = length(matched.to))
+    #Weights; 1 for units that receive a match, 1/ratio for each time unit is
+    #used as a match for someone else
+    w <- as.numeric(!is.na(matched.to[,1])) + tabulate(matched.to, nbins = nrow(matched.to))/ncol(matched.to)
 
-    dataset[[weights]] <- w
+    data[[weights]] <- w
 
     if (!with_replacement) {
-      matched <- which(!is.na(matched.to))
+      matched <- which(!is.na(matched.to[,1]))
 
       s <- factor(matched)
       levels(s) <- seq_len(nlevels(s))
 
-      dataset[[subclass]] <- factor(NA, levels = levels(s))
+      data[[subclass]] <- factor(NA, levels = levels(s))
 
-      dataset[[subclass]][matched] <- s
-      dataset[[subclass]][matched.to[matched]] <- s
+      data[[subclass]][matched] <- s
+
+      for (i in seq_len(ncol(matched.to))) {
+        data[[subclass]][matched.to[matched, i]] <- s
+      }
+
     }
-    dataset <- dataset[w > 0,]
+    data <- data[w > 0,]
 
-    attr(dataset, "weights") <- weights
-    if (!with_replacement) attr(dataset, "subclass") <- subclass
+    attr(data, "weights") <- weights
+    if (!with_replacement) attr(data, "subclass") <- subclass
   }
-  return(dataset)
+  return(data)
 }
