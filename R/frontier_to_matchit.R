1bbc2afe30276e1e24d5a6f885b79c23420dab7c
Noah Greifer <noah.greifer@gmail.com>
2022-08-26 12:56:56 -0400

    Improvements to accommodate matrix matched.to and data

--- R/frontier_to_matchit.R
+++ R/frontier_to_matchit.R
@@ -34,31 +34,41 @@ frontier_to_matchit <- function(frontier.object, N, Ndrop) {
                      "SATT" = "ATT",
                      "FSATT" = "ATT")
 
-  lab <- rownames(frontier.object$dataset)
-  treat <- setNames(frontier.object$dataset[[frontier.object$treatment]], lab)
-
+  lab <- rownames(frontier.object$data)
+  treat <- setNames(frontier.object$data[[frontier.object$treatment]], lab)
 
   if (!is.null(frontier.object$matched.to)) {
-    is.na(frontier.object$matched.to)[drop.inds] <- TRUE
+    is.na(frontier.object$matched.to)[drop.inds,] <- TRUE
 
-    match.matrix <- matrix(lab[frontier.object$matched.to], ncol = 1,
-                           dimnames = list(lab, NULL))
+    mm <- frontier.object$matched.to
+    rownames(mm) <- lab
 
-    weights <- as.numeric(!is.na(frontier.object$matched.to)) + tabulate(frontier.object$matched.to, nbins = length(frontier.object$matched.to))
+    weights <- as.numeric(!is.na(frontier.object$matched.to[,1])) + tabulate(frontier.object$matched.to, nbins = nrow(frontier.object$matched.to))/ncol(frontier.object$matched.to)
     names(weights) <- lab
+
+    if (estimand == "ATT") {
+      mm <- mm[treat == 1,, drop = FALSE]
+    }
+
+    match.matrix <- array(NA_character_, dim = dim(mm), dimnames = dimnames(mm))
+    match.matrix[] <- lab[mm]
+    rm(mm)
   }
   else {
     match.matrix <- NULL
-    weights <- setNames(rep(1, nrow(frontier.object$dataset)), lab)
+    weights <- setNames(rep(1, nrow(frontier.object$data)), lab)
     weights[drop.inds] <- 0
   }
 
   method <- "matchingFrontier"
   attr(method, "method") <- paste0("matching frontier (minimizing the ", metric2info(frontier.object$metric), ")")
   info <- list(method = method,
+               distance = NULL,
                replace = if (inherits(frontier.object, "distFrontier")) TRUE else NULL,
+               ratio = if (inherits(frontier.object, "distFrontier")) ncol(match.matrix) else NULL,
                mahalanobis = FALSE,
                distance_is_matrix = frontier.object$metric == "custom")
+
   # if (inherits(frontier.object, "distFrontier")) {
   #   info$distance <- "user"
   #   attr(info$distance, "custom") <- switch(frontier.object$metric,
@@ -70,7 +80,7 @@ frontier_to_matchit <- function(frontier.object, N, Ndrop) {
   out <- list(
     match.matrix = match.matrix,
     weights = weights,
-    X = frontier.object$dataset[frontier.object$match.on],
+    X = frontier.object$data[frontier.object$match.on],
     call = frontier.object$call,
     info = info,
     estimand = estimand,
